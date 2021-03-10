98ece4c44c0c26a5d85407a18dcd25abf2c5d7f7
xld <xld@nagoya.cc>
2021-03-11 00:12:00 +0900

    編集、削除機能追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
 class TweetsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_tweets, only: [:show, :edit, :update, :destroy]
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
   def new
@@ -9,8 +9,48 @@ class TweetsController < ApplicationController
   end
 
   def show
-
   end
 
+  def edit
+  end
 
+  def create
+    @post = Post.new(tweet_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to tweets_path, notice: "投稿完了"
+        else
+          render :new
+        end
+      end
+  end
+
+  def update
+    if @post.update(tweet_params)
+      redirect_to tweets_path, notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to tweets_path, notice:"削除しました！"
+  end
+
+  def confirm
+    @post = Post.new(tweet_params)
+    render :new if @post.invalid?
+  end
+
+  private
+  def tweet_params
+    params.require(:post).permit(:content)
+  end
+
+  def set_tweets
+    @post = Post.find(params[:id])
+  end
 end
